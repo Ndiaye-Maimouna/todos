aba11e710f1c4e9b4e7c6a202a17cb772a6e2405
Maïmouna Ndiaye <ndiayemaimouna@ept.sn>
2025-07-15 16:34:19 +0000

    modifications data-2

--- src/test/java/sn/ept/git/seminaire/cicd/data/TagTestData.java
+++ src/test/java/sn/ept/git/seminaire/cicd/data/TagTestData.java
@@ -1,19 +1,17 @@
-//package sn.ept.git.seminaire.cicd.data;
-//
-//
-//import sn.ept.git.seminaire.cicd.entities.Tag;
-//
-//import java.time.LocalDateTime;
-//
-//public final class TagTestData extends TestData {
-//
-//    public static Tag defaultTag() {
-//        return Tag.builder()
-//                .id(Default.id)
-//                .createdDate(Default.createdDate)
-//                .lastModifiedDate(Default.lastModifiedDate)
-//                .version(Default.version)
-//                .name(Default.name)
-//                .build();
-//    }
-//}
+package sn.ept.git.seminaire.cicd.data;
+
+
+import sn.ept.git.seminaire.cicd.entities.Tag;
+
+public final class TagTestData extends TestData {
+
+    public static Tag defaultTag() {
+        return Tag.builder()
+                .id(Default.id)
+                .createdDate(Default.createdDate)
+                .lastModifiedDate(Default.lastModifiedDate)
+                .version(Default.version)
+                .name(Default.name)
+                .build();
+    }
+}
